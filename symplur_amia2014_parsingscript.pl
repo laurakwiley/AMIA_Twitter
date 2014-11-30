acff325273cdddeb6f2bc65ea300d2d9e0472535
Laura Wiley <laura@git.com>
2014-11-30 13:17:41 -0600

    Updated data files and analysis
    Updated datafiles and analysis scripts to account for 2014 data.

--- symplur_amia2014_parsingscript.pl
+++ symplur_amia2014_parsingscript.pl
@@ -2,7 +2,7 @@ my $inputfile="symplur_amia2014_raw.txt";
 open(INPUT_FILE, "<$inputfile");
 open(OUT_FILE, ">symplur_amia2014_clean.txt");
 while(<INPUT_FILE>){
- if($.%2!=1){$_=~s/\n/\t/;}
+ if($.%2!=1){$_=~s/\n//;}
  $_=~s/ReplyRetweetFavorite//;
  $_=~s/\t\t/\t/;
  if($.==1){next}
